9372f8ed58ec45ef68272952e00621d3729584a9
dipaCtsGit <dipa.mukherjee@cognizant.com>
2024-07-22 19:59:24 +0530

    Add files via upload

---
+++ AllFunctions.svc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Test
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AllFunctions" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select AllFunctions.svc or AllFunctions.svc.cs at the Solution Explorer and start debugging.
+    public class AllFunctions : IAllFunctions
+    {
+        public void DoWork()
+        {
+        }
+
+        public string fn_GetRemainder(float x, float y)
+        {
+            try
+            {
+                float r;
+
+                if (x == 0 || y == 0)
+                {
+                    return "Cannot divide by zero.";
+                }
+                else
+                {
+                    if (x > y)
+                        r = x % y;
+                    else
+                        r = y % x;
+
+                    return "The remainder is : " + r;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return "Error in" + ex.Message;
+            }
+        }
+
+        public string fn_GetPrime(int x)
+        {
+            try
+            {
+                int i = 0;
+                int count = 0;
+                for ( i = 1; i <= x; i++)
+                {
+                    if (x % i ==0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                    return x + " is a prime number."; 
+                else
+                    return x + " is not a prime number.";
+
+            }
+            catch (Exception ex)
+            {
+                return "Error in" + ex.Message;
+            }
+        }
+    }
+}
---
+++ IAllFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Test
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAllFunctions" in both code and config file together.
+    [ServiceContract]
+    public interface IAllFunctions
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        string fn_GetRemainder(float x, float y);
+
+        [OperationContract]
+        string fn_GetPrime(int x);
+    }
+}
